765adaeeaec91ca691629971c5314d6182fe96e7
Pat Downey <pat.downey@gmail.com>
2016-07-08 14:36:18 +0100

    add collectd test stub - don't copy with plugin

---
+++ collectd.py
@@ -0,0 +1,40 @@
+import time
+
+class ConfigNode:
+  def __init__(self, key, values):
+    self.key = key
+    self.values = values
+
+class Config:
+  def __init__(self):
+    self.children = []
+
+  def add(self, key, value):
+    self.children.append(ConfigNode(key,[value]))
+
+class Values():
+  def __init__(self):
+    self.plugin = None
+    self.plugin_instance = None
+    self.type = None
+    self.type_instance = None
+    self.values = []
+
+  def dispatch(self):
+    for v in self.values:
+      print "...{}-{}.{}-{} {}".format(self.plugin, self.plugin_instance, self.type, self.type_instance, v)
+
+def warning(message):
+  print "{}".format(message)
+
+def register_read(plugin_query_func):
+  plugin_query_func()
+  print "..."
+
+def register_config(plugin_config):
+  test_config = Config()
+  test_config.add('Host', '192.168.99.100')
+  test_config.add('Port', '37268')
+  #test_config.add('ReplSet', 'pats-replset')
+
+  plugin_config(test_config)
